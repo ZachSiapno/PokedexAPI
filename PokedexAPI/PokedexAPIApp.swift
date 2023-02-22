8761f46db8f08b9ef9d759dfac9605b7c94ee729
ZachSiapno <ewsb52@gmail.com>
2023-02-22 10:57:47 -0500

    Initial Commit

---
+++ PokedexAPI/PokedexAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAPIApp.swift
+//  PokedexAPI
+//
+//  Created by Zachary Siapno on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
